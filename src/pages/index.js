1cb7b61680361c3ee1d8a862dcec9c63f251b03a
Rupa Jangala <rupa@pixlcoders.com>
2018-08-04 00:34:50 +0530

    Testing

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Love you Nikhil</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/"> Test File</Link>
+  </div>
+)
+
+export default IndexPage
